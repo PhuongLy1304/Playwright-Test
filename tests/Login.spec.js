f9312a3ebd2008ab6bde00c84e8cc16fc60dd8f0
PhuongLy1304 <phuonglyvvvv@gmail.com>
2024-10-15 09:47:05 +0700

    Selenium

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -111,12 +111,62 @@ test('TC-Login-06:Username =31', async({page})=>{
   await page.locator('button:Has-text("Đăng nhập")').click();
   const actualResult = await page.locator('div.ant-form-item-explain-error');
   await expect(actualResult).toHaveText("Độ dài tên đăng nhập phải nằm trong khoảng 3 đến 30 ký tự");
-})
-
-
+});
 
 
 // Viết test cases để kiểm thử: Mật khẩu >= 6 và <=10 ký tự
-// Viết test cases để kiểm thử: Khi nhập sai Tên đăng nhập hoặc Mật khẩu: "Tên đăng nhập hoặc Mật khẩu đã nhập sai"
+// TestCase 7 : Khi pass = 6 kí tự
+test('TC-Login -07: Password =6', async ({ page }) => {
+  await page.locator('#login-form_username').fill("admin");
+  await page.locator('#login-form_password').fill('Tester');
+  await page.locator('button:Has-text("Đăng nhập")').click();
+  const actualResult=await page.locator('div.ant-form-item-explain-error');
+  await expect(actualResult).not.toBeInViewport();
+});
+
+//Test Case 8: Khi pass = 7 kí tự
+test('TC -Login -08 :Password = 7', async ({ page }) => {
+  await page.locator('#login-form_username').fill("admin");
+  await page.locator('#login-form_password').fill('Tester@');
+  await page.locator('button:Has-text("Đăng nhập")').click();
+  const actualResult=await page.locator('div.ant-form-item-explain-error');
+  await expect(actualResult).not.toBeInViewport();
+});
+
+//Test Case 9: Khi Pass = 5 kí tự
+test('TC - Login - 09: Password =5', async ({ page }) => {
+   await page.locator('#login-form_username').fill("admin");
+   await page.locator("#login-form_password").fill("Test1");
+   await page.locator('button:Has-text("Đăng nhập")').click();
+   const actualResult = await page.locator('div.ant-form-item-explain-error');
+   await expect(actualResult).toHaveText("Độ dài mật khẩu phải nằm trong khoảng 6 đến 10 ký tự");
+});
+//Test Case 10 : khi pass = 10 ký tự
+test('TC -Login -10 :Password = 10', async ({ page }) => {
+  await page.locator('#login-form_username').fill("admin");
+  await page.locator('#login-form_password').fill('Tester@129');
+  await page.locator('button:Has-text("Đăng nhập")').click();
+  const actualResult=await page.locator('div.ant-form-item-explain-error');
+  await expect(actualResult).not.toBeInViewport();
+});
+//Test case 11: khi pass = 9 ký tự
+test('TC -Login -11 :Password = 9', async ({ page }) => {
+  await page.locator('#login-form_username').fill("admin");
+  await page.locator('#login-form_password').fill('Tester@12');
+  await page.locator('button:Has-text("Đăng nhập")').click();
+  const actualResult=await page.locator('div.ant-form-item-explain-error');
+  await expect(actualResult).not.toBeInViewport();
+});
+//Test case 12: khi pass = 11 ký tự
+test('TC - Login - 12: Password =11', async ({ page }) => {
+  await page.locator('#login-form_username').fill("admin");
+  await page.locator("#login-form_password").fill("Test1");
+  await page.locator('button:Has-text("Đăng nhập")').click();
+  const actualResult = await page.locator('div.ant-form-item-explain-error');
+  await expect(actualResult).toHaveText("Độ dài mật khẩu phải nằm trong khoảng 6 đến 10 ký tự");
+});
+
+
+
 
 
